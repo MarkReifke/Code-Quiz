61813b72eff6384a7af1c7cc030f3730e4a1fcfa
Mark Reifke <mark.reifke@gmail.com>
2021-03-03 09:32:17 -0700

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,34 @@ var questions = [
         correctAnswer: "Millennium Falcon",
     },
 ];
+var questions = [
+    {
+        text: "In what month were all six original Star Wars films released?",
+        answers: ["March", "June", "July", "May"],
+        correctAnswer: "May",
+    },
+];
+var questions = [
+    {
+        text: "Which Star Wars character is partially named after director George Lucas’ son?",
+        answers: ["Rick olie", "Willrow Hood", "Dexter Jettster", "Max Rebo"],
+        correctAnswer: "Dexter Jettster",
+    },
+];
+var questions = [
+    {
+        text: "Which species stole the plans to the Death Star?",
+        answers: ["Sullustan", "Bothan", "Rakata", "Cerean"],
+        correctAnswer: "Bothan",
+    },
+];
+var questions = [
+    {
+        text: "How many languages is C-3PO fluent in?",
+        answers: ["Over eight million", "Over seven million", "Over six million", "Over five million"],
+        correctAnswer: "Over six million",
+    },
+];
 
 $startBtn.addEventListener("click", function (e) {
     $timer.textContent = time;
@@ -28,6 +56,7 @@ $questionText.textContent = question[0].text;
 question[0].answers.forEach(function(item){
     var $answerBtn = document.createElement("button");
     $answerBtn.textContent = item;
+    $answerBtn.addEventListener("click", handleAnswerClick);
     $answer.append($answerBtn);
 
 });
