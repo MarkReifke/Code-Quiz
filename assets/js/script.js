b29e9cf1f740a948c71720fa8c9b6556a61b01b9
Mark Reifke <mark.reifke@gmail.com>
2021-03-04 11:44:46 -0700

    nothing works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,95 @@
-var $startBtn = document.querySelector("#start");
-var $start = document.querySelector("#start-prompt");
+var $startBtn = document.querySelector("#startbtn");
+var $statPrompt = document.querySelector("#start-prompt");
+var $questionPrompt = document.querySelector("#question-prompt")
 var $timer = document.querySelector("#timer");
 var $questionText = document.querySelector("#question-Text");
 var $answer = document.querySelector("#answer");
+var $questionOptions = document.querySelector("#options")
 var time = 200;
+
+var timerInterval;
+var currentQuestion = 0;
+
+$startBtn.addEventListener("click", function (e) {
+    $statPrompt.classList.add("hide");
+    $questionPrompt.classList.remove("hide");
+    $timer.classList.remove("hide");
+}
+
+function renderQuestion() {
+    $questionOptions.innerHTML = "";
+    $questionText.textContent = questions[currentQuestion].text;
+    questions[currentQuestion].options.forEach(function (items) {
+        var $btn = document.createElement("button");
+        $btn.textContent = item;
+        $questionOptions.append($btn);
+    });
+}
+
+$questionOptions.addEventListener("click", function (e) {
+    if (!e.target.matches("button")) return;
+
+    var val = e.target.textContent;
+    if (val === questions[currentQuestion].correctAnswer) {
+        console.log("Correct! Great Job");
+    } else {
+        console.log("Sorry That isnt Correct")
+    }
+
+    currentQuestion++;
+
+    if(currentQuestion === questions.length){     
+    } else {
+        renderQuestion();
+    }
+});
 
 var questions = [
     {
-        text: "What is the name of Han Solo’s ship?",
-        answers: ["Millennium Falcon", "Flagship", "Empire Ship", "Millennium Cruiser"],
-        correctAnswer: "Millennium Falcon",
+        question: "What is the name of Han Solo’s ship?",
+        A: "Millennium Falcon",
+        B: "Flagship",
+        C: "Empire Ship",
+        D: "Millennium Cruiser",
+        Answer: "A" 
     },
-];
-var questions = [
     {
-        text: "In what month were all six original Star Wars films released?",
-        answers: ["March", "June", "July", "May"],
-        correctAnswer: "May",
+        question: "In what month were all six original Star Wars films released?",
+        A: "March",
+        B: "June",
+        C: "July",
+        D: "May",
+        Answer: "D" 
     },
-];
-var questions = [
     {
-        text: "Which Star Wars character is partially named after director George Lucas’ son?",
-        answers: ["Rick olie", "Willrow Hood", "Dexter Jettster", "Max Rebo"],
-        correctAnswer: "Dexter Jettster",
+        question: "Which Star Wars character is partially named after director George Lucas’ son?",
+        A: "Rick olie",
+        B: "Willrow Hood",
+        C: "Dexter Jettster",
+        D: "Max Rebo",
+        Answer: "C" 
     },
-];
-var questions = [
     {
-        text: "Which species stole the plans to the Death Star?",
-        answers: ["Sullustan", "Bothan", "Rakata", "Cerean"],
-        correctAnswer: "Bothan",
+        question: "Which species stole the plans to the Death Star?",
+        A: "Sullustan",
+        B: "Bothan",
+        C: "Rakata",
+        D: "Cerean",
+        Answer: "B" 
     },
-];
-var questions = [
     {
-        text: "How many languages is C-3PO fluent in?",
-        answers: ["Over eight million", "Over seven million", "Over six million", "Over five million"],
-        correctAnswer: "Over six million",
+        question: "How many languages is C-3PO fluent in?",
+        A: "Over eight million",
+        B: "Over seven million",
+        C: "Over six million",
+        D: "Over five million",
+        Answer: "C" 
     },
 ];
+highScoreBtn.addEventListener("click", highScoreDisplay);
+function highScoreDisplay () {
+    console.log("High Score");
+}
 
 $startBtn.addEventListener("click", function (e) {
     $timer.textContent = time;
@@ -63,4 +113,3 @@ question[0].answers.forEach(function(item){
 
 
 
-});
